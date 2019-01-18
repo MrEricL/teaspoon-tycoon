54e72766b8be95c1b2adaff3740028bc71c2d4ce
Jeffrey Lin <jeffrey@icurse.nl>
2019-01-18 15:39:49 -0500

    Add Webpack and friends

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: './client/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        type: 'javascript/esm',
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '*',
+      '.js',
+      '.jsx',
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    port: 9000,
+  },
+}
