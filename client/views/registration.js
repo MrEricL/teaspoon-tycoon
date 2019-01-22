0127381771f0fc95aa5e002aa602cde38da6f43e
Jeffrey Lin <jeffrey@icurse.nl>
2019-01-22 00:21:32 -0500

    client: update registration view to match model
    Add country, postal code, and town fields

--- client/views/registration.js
+++ client/views/registration.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { COUNTRIES } from '../data/countries';
 import { postInternal } from '../common';
 import { TextBox } from './common';
 
@@ -23,6 +24,17 @@ class RegistrationForm extends Component {
         <TextBox type="text" name="name" label="Name" />
         <TextBox type="text" name="email" label="Email" />
         <TextBox type="password" name="pass" label="Password" />
+
+        <label htmlFor="country">Country</label>
+        <select name="country" id="country">
+          {Object.entries(COUNTRIES).map(x => (
+            <option value={x[0]}>{x[1]}</option>
+          ))}
+        </select>
+
+        <TextBox type="number" name="postal" label="Postal Code" />
+        <TextBox type="text" name="town" label="Town" />
+
         <p>I am a</p>
         <TextBox type="radio" value="person" name="porb" set={true}/>
         <TextBox type="radio" value="bank" name="porb" />
